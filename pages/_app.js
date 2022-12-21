bdcde211d2b92347c69058135937dcef969395e2
Cole Peterson <rcolepeterson@gmail.com>
2022-12-21 11:18:53 -0800

    google and local fonts working

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,30 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { Roboto } from "@next/font/google";
+import localFont from "@next/font/local";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const roboto = Roboto({
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+});
+
+const hsbold = localFont({
+  src: "./fonts/HungerStation-Regular.woff2",
+});
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <main className={hsbold.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
+
+// google fonts working.
+// export default function App({ Component, pageProps }) {
+//   return (
+//     <main className={roboto.className}>
+//       <Component {...pageProps} />
+//     </main>
+//   );
+// }
